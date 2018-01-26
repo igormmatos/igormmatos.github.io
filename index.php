e340dd15b7a115cec8e9820125a91fac7d53e832
Igor Macedo <igorantonio50@hotmail.com>
2018-01-26 10:29:09 -0200

    Adicionando Arquivos

---
+++ index.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title></Igor M.></title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+</head>
+<style>
+body{
+  font-family: 'Roboto', sans-serif;
+  font-size: 15px;
+  line-height: 1.5;
+}
+hr {
+  border: 0;
+  border-top: 1px solid #eee;
+  margin: 20px 0;
+  box-sizing: content-box;
+  height: 0;
+  overflow: visible;
+}
+a{
+  color: #fff;
+}
+a:hover{
+  text-decoration: none;
+  color: #8c8c8c;
+}
+</style>
+<body>
+  <div class="container-fluid bg-secondary">
+    <div class="row">
+      <div class="col-md-4 pt-3">
+        <div class="card bg-dark text-light">
+          <div class="card-body">
+            <div class="row">
+              <p><i class="fa fa-male" style="font-size:18px; margin-left: 20px; margin-right: 12px"></i>
+                Igor Antônio Macedo de Matos
+              </p>
+            </div>
+            <div class="row">
+              <p><i class="fa fa-briefcase ml-3 mr-2" style="font-size:18px"></i>
+                Desenvolvedor
+              </p>
+            </div>
+            <div class="row">
+              <p><i class="fa fa-home ml-3 mr-2" style="font-size:18px"></i>
+                Belo Horizonte, MG
+              </p>
+            </div>
+            <div class="row">
+              <p><i class="fa fa-envelope ml-3 mr-2" style="font-size:18px"></i>
+                igorantonio50@hotmail.com
+              </p>
+            </div>
+            <div class="row">
+              <p><i class="fa fa-phone ml-3 mr-2" style="font-size:18px"></i>
+                (31) 9.98642-3797
+              </p>
+            </div>
+            <hr />
+            <div class="row">
+              <p><i class="fa fa-code ml-3 mr-2" style="font-size:18px"></i>
+                <strong class="h5">Habilidades</strong>
+              </p>
+            </div>
+            <div class="row">
+              <p class="ml-3 mr-2">
+                <strong>HTML - PHP - SQL</strong>
+              </p>
+            </div>
+            <div class="progress border mb-2">
+              <div class="progress-bar bg-dark" style="width:80%">Intermediário - Avançado</div>
+            </div>
+            <div class="row">
+              <p class="ml-3 mr-2">
+                <strong>C# - JavaScript - JQuery - AJAX </strong>
+              </p>
+            </div>
+            <div class="progress border mb-2">
+              <div class="progress-bar bg-dark" style="width:60%">Intermediário</div>
+            </div>
+            <div class="row">
+              <p class="ml-3 mr-2">
+                <strong>CSS</strong>
+              </p>
+            </div>
+            <div class="progress border mb-2">
+              <div class="progress-bar bg-dark" style="width:40%">Básico - Intermediário</div>
+            </div>
+            <div class="row">
+              <p class="ml-3 mr-2">
+                <strong>Instalação/Configuração Softwares e Sistemas Operacionais</strong>
+              </p>
+            </div>
+            <div class="progress border mb-2">
+              <div class="progress-bar bg-dark" style="width:80%">Intermediário - Avançado</div>
+            </div>
+            <div class="row">
+              <p class="ml-3 mr-2">
+                <strong>Operar Pacote Office</strong>
+              </p>
+            </div>
+            <div class="progress border mb-2">
+              <div class="progress-bar bg-dark" style="width:80%">Intermediário - Avançado</div>
+            </div>
+            <div class="row">
+              <p class="ml-3 mr-2">
+                <strong>Operar S.O. Windows/Linux</strong>
+              </p>
+            </div>
+            <div class="progress border mb-2">
+              <div class="progress-bar bg-dark" style="width:60%">Intermediário</div>
+            </div>
+            <div class="row">
+              <p class="ml-3 mr-2">
+                <strong>Hardware</strong>
+              </p>
+            </div>
+            <div class="progress border mb-2">
+              <div class="progress-bar bg-dark" style="width:40%">Básico - Intermediário</div>
+            </div>
+            <div class="row">
+              <p><i class="fa fa-globe ml-3 mr-2 mt-2" style="font-size:18px"></i>
+                <strong class="h5">Idiomas</strong>
+              </p>
+            </div>
+            <div class="row">
+              <p class="ml-3 mr-2">
+                <strong>Inglês</strong>
+              </p>
+            </div>
+            <div class="progress border mb-2">
+              <div class="progress-bar bg-dark" style="width:40%">Básico - Intermediário</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-8 pt-3">
+        <div class="card bg-dark text-light">
+          <div class="card-body">
+            <div class="row">
+              <p style="font-size:28px">
+                <i class="fa fa-suitcase ml-3 mr-3 mt-2" style="font-size:36px"></i>
+                Experiência Profissional
+              </p>
+            </div>
+            <div class="row">
+              <p class="ml-5 mt-2" style="font-size:18px">
+                <strong>Estagiário / Colégio /<a href="https://www.cotemig.com.br/" target="_blank"> ©COTEMIG</a></strong>
+              </p>
+            </div>
+            <div class="row">
+              <p class="ml-5" style="font-size:16px">
+                <i class="fa fa-calendar ml-1 mr-3 mt-2" style="font-size:22px"></i>
+                Mai 2017 - Dez 2017
+              </p>
+            </div>
+            <div class="row">
+              <p class="ml-5">
+                <i class="fa fa-envelope ml-1 mr-3 mt-2" style="font-size:22px"></i>
+                Referências - <a href="mailto:leonardo@cotemig.com.br?Subject=Referências%20Igor%20Estagiário" target="_top">Leonardo Fonseca</a>.
+              </p>
+            </div>
+            <div class="row">
+              <p class="ml-5">
+                Atuei ajudando os alunos, quando tinham alguma dúvida relacionada a programação. E auxiliava os professores durante as aulas. Além de dar monitoria aos alunos após as aulas.
+              </p>
+            </div>
+            <hr />
+          </div>
+        </div>
+        <div class="card bg-dark text-light mt-3">
+          <div class="card-body">
+            <div class="row">
+              <p style="font-size:28px">
+                <i class="fa fa-mortar-board ml-3 mr-3 mt-2" style="font-size:36px"></i>
+                Formação
+              </p>
+            </div>
+            <div class="row">
+              <p class="ml-4 mt-2" style="font-size:18px">
+                <strong>Técnico em Informática /<a href="https://www.cotemig.com.br/" target="_blank"> ©COTEMIG</a></strong>
+              </p>
+            </div>
+            <div class="row">
+              <p class="ml-4" style="font-size:16px">
+                <i class="fa fa-calendar ml-1 mr-3 mt-2" style="font-size:22px"></i>
+                Jan 2016 - Dez 2017
+              </p>
+            </div>
+            <div class="row">
+              <p class="ml-4">
+                Aluno do colégio <a href="https://www.cotemig.com.br/" target="_blank"> ©COTEMIG </a> - Unidade BARROCA, no turno da manhã. Com conclusão em Dezembro de 2017.
+              </p>
+            </div>
+            <hr />
+          </div>
+        </div>
+        <div class="card bg-dark text-light mt-3">
+          <div class="card-body">
+            <div class="row">
+              <p style="font-size:28px">
+                <i class="fa fa-bullseye ml-3 mr-3 mt-2" style="font-size:36px"></i>
+                Diferencial
+              </p>
+            </div>
+            <div class="row">
+              <p class="ml-4 mt-2">
+                Participante, como expositor, na primeira e segunda edição da FINIT (<i>Feira Internacional de Negócios, Inovação e Tecnologia</i>) no Expominas. – A maior feira de tecnologia em Minas Gerais.
+              </p>
+              <p class="ml-4">
+                <a href="arquivos/certificados.pdf" target="_blank" style="text-decoration: underline;"> CLIQUE AQUI PARA VER OS CERTIFICADOS </a>
+              </p>
+            </div>
+            <hr />
+          </div>
+        </div>
+        <div class="card bg-dark text-light mt-3">
+          <div class="card-body">
+            <div class="row">
+              <p style="font-size:28px">
+                <i class="fa fa-file-code-o ml-3 mr-3 mt-2" style="font-size:36px"></i>
+                Projetos
+              </p>
+            </div>
+            <div class="row">
+              <p class="ml-4 mt-2">
+                Aqui temos projetos desenvolvidos por mim para passar o tempo, aprender uma tecnologia nova, e alguns desenvolvidos ao longo do meu curso no colégio ©COTEMIG.
+              </p>
+              <p class="ml-4">
+                <a href="arquivos/certificados.pdf" target="_blank" style="text-decoration: underline;"> CLIQUE AQUI PARA VER OS CERTIFICADOS </a>
+              </p>
+            </div>
+            <hr />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12 text-center">
+        Teste
+      </div>
+    </div>
+  </div>
+</body>
+</html>
